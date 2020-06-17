b1e0a56d5b3921f8078d8be74ee925ffb12b7a05
Yuvraj Singh Babrah <yuvrajb@users.noreply.github.com>
2020-06-18 04:22:30 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: "http://data.fixer.io/api/",
-  apiKey: "8d72b948a4b27a075ba25afc29939078"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 };
